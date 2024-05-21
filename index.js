9bca18242b90f0939f968593e8daa72cfd1fcebb
NeerajYadav0 <neerajrao04@gmail.com>
2024-05-21 22:41:10 +0530

    some changes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const multer = require("multer");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const jwt = require("jsonwebtoken");
@@ -13,14 +12,24 @@ const otpRoute = require("./routes/otp");
 const commonRoute = require("./routes/common");
 const razorPayRoute = require("./routes/payment");
 
-//configurations
+// Configurations
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(cors({ credentials: true }));
+
+// CORS options
+const corsOptions = {
+  origin: ["https://legal-vise-client.vercel.app", "http://localhost:5173"], // Allow your frontend's origin
+  methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+  credentials: true, // Allow credentials
+};
+
+// Use the CORS middleware
+app.use(cors(corsOptions));
 
 app.use("/api/customer", customerRoute);
 app.use("/api/serviceProvider", serviceProviderRoute);
@@ -28,15 +37,6 @@ app.use("/api/jobs", jobRoute);
 app.use("/api/otp", otpRoute);
 app.use("/api/common", commonRoute);
 app.use("/api/razorpay", razorPayRoute);
-
-const corsOptions = {
-  origin: ["https://legal-vise-client.vercel.app/", "http://localhost:5173/"], // Allow your frontend's origin
-  methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
-};
-
-// Use the CORS middleware
-app.use(cors(corsOptions));
 
 const port = process.env.PORT || 8000;
 mongoose.set("strictQuery", true);
